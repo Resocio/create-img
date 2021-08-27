7f6b40cd460c75c054dabfaa432e465867aed09d
Philippe Bernard <philippe.bernard@gmail.com>
2021-08-27 16:57:04 +0200

    Cosmetic changes in command help

--- src/cli.ts
+++ src/cli.ts
@@ -8,9 +8,9 @@ const runCompiler = async () => {
     .name('create-img')
     .description("Create an image based on a Resoc image template")
     .version(require('../package.json').version)
-    .argument('[manifest]', 'Path to the template manifest', (v, p) => v, './image-template-manifest.json')
-    .option('-p, --params <parameters...>', 'Parameter values, with <name>=<value> format')
-    .option('-o, --output <imagePath>', 'Output image file', './output.png')
+    .argument('[manifest path]', 'path of the image template manifest', (v, p) => v, './image-template-manifest.json')
+    .option('-p, --params <parameters...>', 'parameter values, with <name>=<value> format')
+    .option('-o, --output <imagePath>', 'output image file', './output.png')
     .action(async (manifestPath, options) => {
       console.log("Create image...");
       await coompileTemplate(manifestPath, options.params, options.output);
