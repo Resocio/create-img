e88b08e2802e302c03676b9f43b30c1fcbaf8bca
Philippe Bernard <philippe.bernard@gmail.com>
2021-08-27 16:44:54 +0200

    Preserve shebangs

--- rollup-cli.config.js
+++ rollup-cli.config.js
@@ -2,6 +2,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import typescript from "rollup-plugin-typescript2";
+import { preserveShebangs } from 'rollup-plugin-preserve-shebangs';
 
 import packageJson from "./package.json";
 
@@ -14,5 +15,5 @@ export default {
       sourcemap: true
     }
   ],
-  plugins: [peerDepsExternal(), commonjs(), typescript(), json()]
+  plugins: [peerDepsExternal(), commonjs(), typescript(), json(), preserveShebangs()]
 };
